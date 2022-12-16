de55d5ac1c03ff2067eae9a0bf4b118de4326d36
Larry <larry40305@gmail.com>
2022-12-16 20:31:27 +0800

    nav

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('food.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=False)
+    
